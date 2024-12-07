ea5471b65594ef96216880bee69d299083341f53
John Doe <johndoe@example.com>
2024-12-07 19:17:02 +0800

    2

---
+++ ai-channel/src/main/java/ChannelBoot.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
+public class ChannelBoot {
+    public static void main(String[] args) {
+        //springboot启动
+        SpringApplication.run(ChannelBoot.class, args);
+
+    }
+}
